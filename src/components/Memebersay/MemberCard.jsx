a0a95a576d04c2605d4f3ac9f3b92562f6fcf99e
da-nazy <danielani660@gmail.com>
2022-08-23 14:15:37 +0100

    feat:membersays

--- src/components/Memebersay/MemberCard.jsx
+++ src/components/Memebersay/MemberCard.jsx
@@ -1,7 +1,95 @@
 import React from 'react'
-
-export default function MemberCard() {
+import {Box,Typography,Rating,Divider} from '@mui/material'
+import obj from '../../assets/image/obj.jpg'
+export default function MemberCard({name,role,img,hdColor,rate}) {
   return (
-    <div>MemberCard</div>
+   <Box sx={style.container}>
+    <Box sx={{...style.smhead, backgroundColor:hdColor?hdColor:"#06113CE5",}}></Box>
+    <Box sx={style.subCont}>
+      
+     <Rating  
+     size='large'
+      name="half-rating-read" defaultValue={rate?rate:2.5} precision={0.5} readOnly
+      sx={style.rating}
+      />
+     <Typography sx={style.desCTxt}>
+      I’ve been a member of this community for over a year and I can say that I,ve learnt alot from this community
+      </Typography>
+      <Divider sx={style.divider}/>
+      <img src={img?img:obj} style={style.img}/>
+      <Typography sx={style.nameTxt}>
+      {name?name:"Oranu Peculiar"}
+      </Typography>
+      <Typography  sx={style.roleTxt}>
+      {role?role:"Product Design Lead"}
+      </Typography>
+    </Box>
+   </Box>
   )
+
+}
+const style={
+  container:{
+  width:'80%',
+  minHeight:'500px',
+  display:'flex',
+  alignItems:'center',
+  flexDirection:'column',
+  },
+  subCont:{
+    width:'100%',
+    borderRadius:'20px',
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    marginTop:'-10px',
+    backgroundColor:'#fff',
+    height:'450px',
+    padding:'20px',
+    boxShadow:"3px 3px 10px 3px rgb(0 0 0 / 0.2)",
+    
+  },
+  desCTxt:{
+    color:"#00000073",
+    fontSize:'18px',
+    fontFamily:'mont',
+    textAlign:'center',
+    marginBottom:'20px'
+  },
+  divider:{
+ minWidth:'158px',
+ borderColor:'#00000080',
+ marginBottom:'27px'
+  },
+  smhead:{
+   
+    width:'97%',
+    borderTopRightRadius:'10px',
+    borderTopLeftRadius:'10px',
+    height:'30px'
+  },
+  rating:{
+    marginBottom:'20px'
+  },
+  img:{
+    width:'200px',
+    height:'200px',
+    borderRadius:'100px',
+    marginBottom:'17px'
+  },
+  nameTxt:{
+fontFamily:"Mont",
+fontSize: "18px",
+fontWeight: 700,
+lineHeight:"23px",
+color:'#06113C',
+marginBottom:'10px'
+  },
+  roleTxt:{
+   color:'#FF8C32',
+   fontFamily:'Mont',
+   fontSize:'12px',
+   lineHeight:'15px',
+   fontWeight:700,
+  }
 }
--- src/components/Memebersay/index.jsx
+++ src/components/Memebersay/index.jsx
@@ -1,14 +1,83 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./carousel.css";
+import { Box,Paper,Typography } from '@mui/material';
+import MemberCard from './MemberCard';
+import oranu from '../../assets/image/oranu.png';
+import jumobi from '../../assets/image/jumobi.png';
 export default function Membersay() {
-  return (
-     <Paper sx={style.container}>
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+      const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
+        const { carouselState: { currentSlide } } = rest;
+        return (
+          <div className="carousel-button-group"> // remember to give it position:absolute
+            <button className={currentSlide === 0 ? 'disable' : ''} onClick={() => previous()} />
+            <button onClick={() => next()} />
+            <button onClick={() => goToSlide(currentSlide + 1)}> Go to any slide </button>
+          </div>
+        );
+      };
 
+  return (
+     <Paper elevation={2} sx={style.container}>
+      <Typography sx={style.wom}>
+      What Our Members Say About Us
+      </Typography>
+      <Box style={{width:'100%'}}>
+      <Carousel 
+      className={'react-multi-carousel-list'}
+      showDots={true}
+      autoPlay={true}
+      responsive={responsive}
+      >
+    <MemberCard rate={4.5} img={oranu}/>
+    <MemberCard rate={5} name="Bakare Jumobi" hdColor="#FF8C32" role="UI / UX Designer" img={jumobi}/>  
+    <MemberCard rate={4} name="Okenwa Sarah" role="CTO, Sapphire" img={oranu}/>
+</Carousel>
+      </Box>
      </Paper>
   )
 }
 const style={
   container:{
-    minHeight:'1000px'
+    minHeight:'500px',
+    width:'100%',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    padding:'20px'
+  },
+  wom:{
+fontFamily: "Mont",
+fontSize:{
+lg:"36px !important",
+md:"30px !important",
+sm:"30px !important",
+xs:"24px !important"
+},
+fontWeight: 700,
+textAlign:'center',
+marginTop:'50px',
+marginBottom:'20px'
   }
 }
